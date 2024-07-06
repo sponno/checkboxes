cb34cb34f43c771d0c97fb6a82eeb20914904006
John <john@goodsign.io>
2024-07-07 11:12:20 +1200

    and server

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -15,7 +15,7 @@ import {
 //   ? 'https://seashell-app-zjonr.ondigitalocean.app'
 //   : 'http://localhost:5001';
 
-const SOCKET_URL = 'https://seashell-app-zjonr.ondigitalocean.app';
+const SOCKET_URL = 'https://seashell-app-zjonr.ondigitalocean.app:443';
 const generateClientId = () => 'client_' + Math.random().toString(36).substr(2, 9);
 
 const GameBoard = () => {
